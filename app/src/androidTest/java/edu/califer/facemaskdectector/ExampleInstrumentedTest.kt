b9d9769b78a6b9043d073e5596012a24e4bb9555
JetBluer <110820395+JetBluer@users.noreply.github.com>
2021-04-12 20:15:18 +0000

    Caching

--- app/src/androidTest/java/edu/califer/facemaskdectector/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/edu/califer/facemaskdectector/ExampleInstrumentedTest.kt
@@ -1,4 +1,6 @@
 package edu.califer.facemaskdectector
 
 import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.ext.junit.runners.Androi
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+im
