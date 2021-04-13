e4c7f9d64ca456273059851645fe716b93a77975
JetBluer <110820395+JetBluer@users.noreply.github.com>
2021-04-13 06:01:02 +0000

    Consistent naming

--- app/src/androidTest/java/edu/califer/facemaskdectector/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/edu/califer/facemaskdectector/ExampleInstrumentedTest.kt
@@ -3,4 +3,5 @@ package edu.califer.facemaskdectector
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
-import org.junit.Tes
+import org.junit.Test
+import org
