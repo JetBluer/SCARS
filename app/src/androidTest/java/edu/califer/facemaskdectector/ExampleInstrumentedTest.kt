e3b0ffba5a5f88b932010d8517484a1ff94e4c03
JetBluer <110820395+JetBluer@users.noreply.github.com>
2021-04-24 13:38:51 +0000

    Updated Makefile

--- app/src/androidTest/java/edu/califer/facemaskdectector/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/edu/califer/facemaskdectector/ExampleInstrumentedTest.kt
@@ -11,4 +11,4 @@ import org.junit.Assert.*
 /**
  * Instrumented test, which will execute on an Android device.
  *
- * See [testing documentation](http://d.android.c
+ * See [testing documentation](http://d.android.com/tools/testing).
