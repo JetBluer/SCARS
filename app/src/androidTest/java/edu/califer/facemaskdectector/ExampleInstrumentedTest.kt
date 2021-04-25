c9cf684672eb90845a95ac10f6a97a14d7d66110
JetBluer <110820395+JetBluer@users.noreply.github.com>
2021-04-25 19:24:38 +0000

    [l10n] Updated German translation

--- app/src/androidTest/java/edu/califer/facemaskdectector/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/edu/califer/facemaskdectector/ExampleInstrumentedTest.kt
@@ -13,4 +13,5 @@ import org.junit.Assert.*
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-@RunWith(AndroidJUnit4::class
+@RunWith(AndroidJUnit4::class)
+class ExampleIn
