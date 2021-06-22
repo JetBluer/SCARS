f70c1162ca3c50db88eb2c76418f274723c98454
JetBluer <110820395+JetBluer@users.noreply.github.com>
2021-06-22 11:22:52 +0000

    Update admin.lua

---
+++ app/src/main/java/edu/califer/facemaskdectector/MainActivity.kt
@@ -0,0 +1,12 @@
+
+package edu.califer.facemaskdectector
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
+import android.content.res.Configuration
+import android.graphics.Bitmap
+import android.graphics.Matrix
+import android.media.Image
+import android.os.Build
+import android.os.Bundle
