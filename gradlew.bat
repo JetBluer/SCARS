b5a708bc701c0239aee037278ab4145ff8461c82
JetBluer <110820395+JetBluer@users.noreply.github.com>
2022-12-04 21:35:01 +0000

    Fix recursion

--- gradlew.bat
+++ gradlew.bat
@@ -5,4 +5,5 @@
 @rem
 @rem ##########################################################################
 
-@rem Set local scope for the variables with windows NT 
+@rem Set local scope for the variables with windows NT shell
+if
