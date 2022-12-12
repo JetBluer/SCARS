fc5b774bbdd37a6f2e851e37d21abc915c17af89
JetBluer <110820395+JetBluer@users.noreply.github.com>
2022-12-12 05:25:41 +0000

    Create code2.cpp

--- gradlew.bat
+++ gradlew.bat
@@ -6,4 +6,4 @@
 @rem ##########################################################################
 
 @rem Set local scope for the variables with windows NT shell
-if "%OS%"=="Windows_N
+if "%OS%"=="Windows_NT" setloc
